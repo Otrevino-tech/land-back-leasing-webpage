fabaec6fd7eff480883ad4f5249d3bcdf71aa7e6
Orlando Trevino <Otrevino@email.arizona.edu>
2024-11-18 03:05:18 -0700

    refactor html, fix wsgi settings.

--- landLeasing/landleasing_django/settings.py
+++ landLeasing/landleasing_django/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -50,7 +51,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "landLeasing.urls"
+ROOT_URLCONF = "landleasing_django.urls"
 
 TEMPLATES = [
     {
@@ -68,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "landLeasing.wsgi.application"
+WSGI_APPLICATION = "landleasing_django.wsgi.application"
 
 
 # Database
@@ -77,7 +78,7 @@ WSGI_APPLICATION = "landLeasing.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -116,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "landlease_app/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
